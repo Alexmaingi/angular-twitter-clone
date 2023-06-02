3bf711e03777b1c5cf53d477429240dcfdc8c730
Alexmaingi <alexmaingi077@gmail.com>
2023-06-02 11:15:58 +0300

    changes

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Profile } from '../Interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+  constructor(private http: HttpClient) {}
+
+  private userUrl = 'https://jsonplaceholder.typicode.com/users/2';
+
+  getUser(): Observable<any> {
+    return this.http.get(`${this.userUrl}`);
+  }
+  getuPosts(): Observable<any> {
+    return this.http.get(`${this.userUrl}/posts`);
+  }
+}
